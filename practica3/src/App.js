7c89584cd20d18a4d746d0016489851a4eefe47b
w Agostina Barbenza <agosbarbenza@gmail.com>
2020-08-29 17:01:33 -0300

    Importé libreria Radium y envolví el return de App.js en <StyleRoot> para aplicar los cambios de mediaqueries

--- practica3/src/App.js
+++ practica3/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import Person from './Person/Person';
+import Radium, { StyleRoot } from 'radium';
 import './styles/styles.css';
-import Radium from 'radium';
+import './styles/Person.css';
+
 
 class App extends Component {
   state = {
     persons: [
-      { id:1, name: 'Max', age: 28 },
-      { id:2, name: 'Manu', age: 29 },
-      { id:3, name: 'Stephanie', age: 26 }
+      { id: 1, name: 'Max', age: 28 },
+      { id: 2, name: 'Manu', age: 29 },
+      { id: 3, name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
     showPersons: false
@@ -17,23 +19,23 @@ class App extends Component {
   switchNameHandler = (newName) => {
     // console.log('Was clicked!');
     // DON'T DO THIS: this.state.persons[0] id:1.name = 'Maximilian';
-    this.setState( {
+    this.setState({
       persons: [
-        { id:1, name: newName, age: 28 },
-        { id:2, name: 'Manu', age: 29 },
-        { id:3, name: 'Stephanie', age: 27 }
+        { id: 1, name: newName, age: 28 },
+        { id: 2, name: 'Manu', age: 29 },
+        { id: 3, name: 'Stephanie', age: 27 }
       ]
-    } )
+    })
   }
 
   nameChangedHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex(p=>{
+    const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
     })
 
     const person = {
       ...this.state.persons[personIndex]
-    
+
     };
 
     person.name = event.target.value;
@@ -41,73 +43,75 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState( {persons: persons} )
+    this.setState({ persons: persons })
   }
 
-  togglePersonsHandler = () =>  {
+  togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons : !doesShow});// cuando hace click cambia el estado de showPersons
+    this.setState({ showPersons: !doesShow });// cuando hace click cambia el estado de showPersons
 
   }
 
-  deletePersonHandler = (personIndex)=>{
+  deletePersonHandler = (personIndex) => {
     //const persons = this.state.persons;
     const persons = [...this.state.persons];
-    persons.splice(personIndex,1);
+    persons.splice(personIndex, 1);
     this.setState(
-      {persons: persons}
+      { persons: persons }
     )
   }
 
-  render () {
+  render() {
     let persons = null;
     const style = {
       backgroundColor: 'green',
       color: 'white',
       border: '1px solid black',
       padding: '8px',
-      cursor : 'pointer',
-      ':hover':{
+      cursor: 'pointer',
+      ':hover': {
         backgroundColor: 'lightgreen',
         color: 'black'
       }
     }
 
-    if(this.state.showPersons){
+    if (this.state.showPersons) {
       persons = (<div>
-        {this.state.persons.map((person, index) =>{
-          return <Person 
-          click={()=>{this.deletePersonHandler(index)}} 
-          name={person.name} 
-          age={person.age}
-          key={person.id}
-          changed={(event)=>{this.nameChangedHandler(event, person.id)}} />
+        {this.state.persons.map((person, index) => {
+          return <Person
+            click={() => { this.deletePersonHandler(index) }}
+            name={person.name}
+            age={person.age}
+            key={person.id}
+            changed={(event) => { this.nameChangedHandler(event, person.id) }} />
         })}
-        </div>)
+      </div>)
 
-        style.backgroundColor = 'red';
-        style[':hover'] = {
-          backgroundColor : 'salmon',
-          color: 'black'
-        }
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     let classes = [];
-    if(this.state.persons.length <=2){
+    if (this.state.persons.length <= 2) {
       classes.push('red');
     }
 
-    if(this.state.persons.length <=1){
+    if (this.state.persons.length <= 1) {
       classes.push('bold');
     }
 
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button onClick={this.togglePersonsHandler} style={style}>Switch Name</button>
-        {persons}        
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button onClick={this.togglePersonsHandler} style={style}>Switch Name</button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
